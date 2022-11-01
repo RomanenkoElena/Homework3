3d0b79dc07de1982c1ce11ae3e7f2b7ccc1bbb19
Elena Romanenko <murenec_elena@mail.ru>
2022-11-01 15:52:25 +0600

    решена задача 21

--- ex21/Program.cs
+++ ex21/Program.cs
@@ -1,21 +1,16 @@
 ﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
 
-int x1 = new Random().Next(-10,10);
-Console.Write($"Ввод координаты x1 {x1}");
-int x2 = new Random().Next(-10,10);
-Console.Write($"Ввод координаты x2 {x2}");
+double x1 = new Random().Next(-10,10);
+double x2 = new Random().Next(-10,10);
+double y1 = new Random().Next(-10,10);
+double y2 = new Random().Next(-10,10);
+double z1 = new Random().Next(-10,10);
+double z2 = new Random().Next(-10,10);
 
-int y1 = new Random().Next(-10,10);
-Console.Write($"Ввод координаты y1 {y1}");
-int y2 = new Random().Next(-10,10);
-Console.Write($"Ввод координаты y2 {y2}");
+Console.WriteLine($"Ввод координат первой точки: x1 - {x1}, y1 - {y1}, z1 - {z1}");
+Console.WriteLine($"Ввод координат второй точки: x2 - {x2}, y2 - {y2}, z2 - {z2}");
 
-int z1 = new Random().Next(-10,10);
-Console.Write($"Ввод координаты z1 {z1}");
-int z2 = new Random().Next(-10,10);
-Console.Write($"Ввод координаты z2 {z2}");
-
-int distance = 0;
-distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z2 - z1, 2));
+double distance = 0;
+distance = Math.Sqrt((x2 - x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2 - z1));
 
 Console.WriteLine($"Расстояние между точками {distance}");
