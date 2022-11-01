3471509970daddf7c34513d2f8fc673401bb8cdd
Elena Romanenko <murenec_elena@mail.ru>
2022-11-01 14:36:01 +0600

    решена задача 19

---
+++ ex19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int numberN = new Random().Next(10000, 99999);
+Console.WriteLine($"Ввод пятизначного числа {numberN}");
+
+int result1 = (numberN / 10000) %10;
+int result2 = (numberN / 1000) % 10;
+int result4 = (numberN / 10) % 10;
+int result5 = numberN % 10;
+
+if (result5 == result1 && result2 == result4)
+{
+    Console.WriteLine($"Да, число {numberN} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Нет, число {numberN} не является палиндромом");
+}
+
+// Console.WriteLine(result1); // эти строки писала для вывода промежуточного результата и проверки работы алгоритма
+// Console.WriteLine(result2);
+// Console.WriteLine(result4);
+// Console.WriteLine(result5);
---
+++ ex21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+int x1 = new Random().Next(-10,10);
+Console.Write($"Ввод координаты x1 {x1}");
+int x2 = new Random().Next(-10,10);
+Console.Write($"Ввод координаты x2 {x2}");
+
+int y1 = new Random().Next(-10,10);
+Console.Write($"Ввод координаты y1 {y1}");
+int y2 = new Random().Next(-10,10);
+Console.Write($"Ввод координаты y2 {y2}");
+
+int z1 = new Random().Next(-10,10);
+Console.Write($"Ввод координаты z1 {z1}");
+int z2 = new Random().Next(-10,10);
+Console.Write($"Ввод координаты z2 {z2}");
+
+int distance = 0;
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z2 - z1, 2));
+
+Console.WriteLine($"Расстояние между точками {distance}");
